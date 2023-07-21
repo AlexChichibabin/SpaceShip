7048b1431a90ec6d1cbf9808f2f89893bab561cb
AlexChichibabin <Wolfrahm57@mail.ru>
2023-07-21 17:02:09 +0300

    Доделать уровень

---
+++ Assets/Scripts/MusicBackground.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SpaceShip
+{
+    public class MusicBackground : SingletonBase<MusicBackground>
+    {
+        private float m_Empty;
+    }
+}
